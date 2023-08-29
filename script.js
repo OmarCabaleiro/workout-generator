d0a49c02fcde48f3d61bf5241743b2bb007368ff
Omar Cabaleiro <51969062+OmarCabaleiro@users.noreply.github.com>
2023-08-28 20:43:11 -0400

    All 5 buttons work.  Did minor style tweaking.

--- script.js
+++ script.js
@@ -1,30 +1,4 @@
-/*const bodyweightArray = ["bodyweight squat", "bodyweight pull", "bodyweight push", "back bend", "forward bend", "bodyweight wild card"];
-const clubbellArray = ["clubbell carry", "clubbell squat", "clubbell overhead", "clubbell lateral", "clubbell wild card"];
-const kettlebellArray = ["kettlebell carry", "kettlebell squat", "kettlebell hinge", "kettlebell pull", "kettlebell push", "kettlebell wild card"];
-const dumbbellArray = ["dumbbell carry", "dumbbell squat", "dumbbell hinge", "dumbbell pull", "dumbbell push", "dumbbell wild card"];
-const barbellArray = ["barbell carry", "barbell squat", "barbell hinge", "barbell pull", "barbell push", "barbell wild card"];*/
 let mainArray = [];
-
-/*function addBodyweight() {
-  mainArray = mainArray.concat(bodyweightArray)
-};
-function addClubbell() {
-  mainArray = mainArray.concat(clubbellArray)
-};
-function addKettlebell() {
-  mainArray = mainArray.concat(kettlebellArray)
-};
-function addDumbbell() {
-  mainArray = mainArray.concat(dumbbellArray)
-};
-function addBarbell() {
-  mainArray = mainArray.concat(barbellArray)
-};
-
-// draw 1 random movement pattern from array
-function draw1() {  
-  output.innerHTML = mainArray[Math.floor(Math.random() * mainArray.length)];
-};*/
 
 // draw 1 random movement pattern from array
 function draw1() {  
@@ -36,6 +10,104 @@ function draw1() {
   output.innerHTML = mainArray[Math.floor(Math.random() * mainArray.length)];
 };
 
+// draw 2 random movement patterns from array
+function draw2() {
+  document.querySelectorAll('[type="checkbox"]').forEach(item => {
+    if (item.checked === true) {
+      mainArray.push(item.value);
+    }
+  })
+  output.innerHTML = mainArray[Math.floor(Math.random() * mainArray.length)] + ",<br>" +
+    mainArray[Math.floor(Math.random() * mainArray.length)];
+};
+
+// draw 3 random movement patterns from array
+function draw3() {
+  document.querySelectorAll('[type="checkbox"]').forEach(item => {
+    if (item.checked === true) {
+      mainArray.push(item.value);
+    }
+  })
+  output.innerHTML = mainArray[Math.floor(Math.random() * mainArray.length)] + ",<br>" +
+    mainArray[Math.floor(Math.random() * mainArray.length)] + ",<br>" +
+    mainArray[Math.floor(Math.random() * mainArray.length)];
+};
+
+// randomly draw 4-10 movement patterns from array, numbered
+function draw4to10() {
+  document.querySelectorAll('[type="checkbox"]').forEach(item => {
+    if (item.checked === true) {
+      mainArray.push(item.value);
+    }
+  })
+  mainArray.sort(function(){return 0.5 - Math.random()});
+  const arr4 = mainArray.slice(0, 4);
+  var result4 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3]
+
+  const arr5 = mainArray.slice(0, 5);
+  var result5 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4]
+  
+  const arr6 = mainArray.slice(0, 6);
+  var result6 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4] + ",<br>" + "6. " + mainArray[5]
+  
+  const arr7 = mainArray.slice(0, 7);
+  var result7 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4] + ",<br>" + "6. " + mainArray[5] + ",<br>" + "7. " + mainArray[6]
+  
+  const arr8 = mainArray.slice(0, 8);
+  var result8 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4] + ",<br>" + "6. " + mainArray[5] + ",<br>" + "7. " + mainArray[6] + ",<br>" + "8. " + mainArray[7]
+  
+  const arr9 = mainArray.slice(0, 9);
+  var result9 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4] + ",<br>" + "6. " + mainArray[5] + ",<br>" + "7. " + mainArray[6] + ",<br>" + "8. " + mainArray[7] + ",<br>" + "9. " + mainArray[8]
+  
+  const arr10 = mainArray.slice(0, 10);
+  var result10 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4] + ",<br>" + "6. " + mainArray[5] + ",<br>" + "7. " + mainArray[6] + ",<br>" + "8. " + mainArray[7] + ",<br>" + "9. " + mainArray[8] + ",<br>" + "10. " + mainArray[9]
+  
+  const arr410 = [result4, result5, result6, result7, result8, result9, result10];
+  output.innerHTML = arr410[Math.floor(Math.random() * arr410.length)];
+};
+
+// randomly draw 1-10 movement patterns from array, numbered
+function draw1to10() {
+  document.querySelectorAll('[type="checkbox"]').forEach(item => {
+    if (item.checked === true) {
+      mainArray.push(item.value);
+    }
+  })
+  mainArray.sort(function(){return 0.5 - Math.random()});
+  const arr1 = mainArray.slice(0, 1);
+  var result1 = "1. " + mainArray[0]
+
+  const arr2 = mainArray.slice(0, 2);
+  var result2 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1]
+
+  const arr3 = mainArray.slice(0, 3);
+  var result3 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2]
+
+  const arr4 = mainArray.slice(0, 4);
+  var result4 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3]
+
+  const arr5 = mainArray.slice(0, 5);
+  var result5 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4]
+  
+  const arr6 = mainArray.slice(0, 6);
+  var result6 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4] + ",<br>" + "6. " + mainArray[5]
+  
+  const arr7 = mainArray.slice(0, 7);
+  var result7 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4] + ",<br>" + "6. " + mainArray[5] + ",<br>" + "7. " + mainArray[6]
+  
+  const arr8 = mainArray.slice(0, 8);
+  var result8 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4] + ",<br>" + "6. " + mainArray[5] + ",<br>" + "7. " + mainArray[6] + ",<br>" + "8. " + mainArray[7]
+  
+  const arr9 = mainArray.slice(0, 9);
+  var result9 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4] + ",<br>" + "6. " + mainArray[5] + ",<br>" + "7. " + mainArray[6] + ",<br>" + "8. " + mainArray[7] + ",<br>" + "9. " + mainArray[8]
+  
+  const arr10 = mainArray.slice(0, 10);
+  var result10 = "1. " + mainArray[0] + ",<br>" + "2. " + mainArray[1] + ",<br>" + "3. " + mainArray[2] + ",<br>" + "4. " + mainArray[3] + ",<br>" + "5. " + mainArray[4] + ",<br>" + "6. " + mainArray[5] + ",<br>" + "7. " + mainArray[6] + ",<br>" + "8. " + mainArray[7] + ",<br>" + "9. " + mainArray[8] + ",<br>" + "10. " + mainArray[9]
+  
+  const arr110 = [result1, result2, result3, result4, result5, result6, result7, result8, result9, result10];
+  output.innerHTML = arr110[Math.floor(Math.random() * arr110.length)];
+};
+
 // select and deselect all buttons
 function selects(){  
   var ele=document.getElementsByName('chk');  
@@ -43,7 +115,7 @@ function selects(){
       if(ele[i].type=='checkbox')  
           ele[i].checked=true;  
   }  
-}  
+};
 function deSelect(){  
   var ele=document.getElementsByName('chk');  
   for(var i=0; i<ele.length; i++){  
@@ -51,19 +123,4 @@ function deSelect(){
           ele[i].checked=false;  
         
   }  
-}
-
-
-
-/*
-get checkbox values
-https://youtu.be/TjpL8U_vxOo?si=BjFty6VHj4PKkDTi
-
-let mainArray = [];
-document.querySelectorAll('[type="checkbox"]').forEach(item => {
-  if (item.checked === true) {
-    mainArray.push(item.value);
-  }
-})
-
-*/
+};
